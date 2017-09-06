54034f60f1e8ab8cd0ac78112430026c8e51d181
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-09-06 16:33:31 +0900

    extraViewにcenterViewをつけた

--- StoryBoadTest/ExtraView.swift
+++ StoryBoadTest/ExtraView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ExtraView: UIView {
 
+    @IBOutlet weak var centerView: UIView!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
