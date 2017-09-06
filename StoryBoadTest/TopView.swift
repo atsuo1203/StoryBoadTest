e9cc7ad239e3cf4326db76e4fe2eff38dbb7f721
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-09-06 16:25:47 +0900

    view作成

---
+++ StoryBoadTest/TopView.swift
@@ -0,0 +1,21 @@
+//
+//  TopView.swift
+//  StoryBoadTest
+//
+//  Created by Atsuo Yonehara on 2017/09/06.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class TopView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
